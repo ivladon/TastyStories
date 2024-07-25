d7a77e2032107a1e141a8b6df194fd2fff893290
Eduard Man <eduardmihoc@gmail.com>
2024-07-25 15:35:45 +0300

    added title overwrite from main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tasty-stories';
+  title = 'Main Page';
+  constructor(private titleService: Title) {
+    this.titleService.setTitle(this.title);
+  }
 }
